0fe56d85ee63e0c57fd393293000ca93ab42ae09
fredsonmartins <fredsonf24@gmail.com>
2025-05-01 17:10:16 +0000

    tentando criar o codigo

---
+++ desafio.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+char estado[30] ="goias";
+char codigodacarta[20] ="G";
+char nome cidade[20] ="G01";
+int Populacao = 3000;
+float area = 1.300;
+float pib = 1.500;
+int pontos turisticos = 600;
+
+
+return o
+
+
+
+
+
+
+
+
+
+
+
+    
+}
