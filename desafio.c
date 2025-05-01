51aed4be692003b0e891b6d598311b2469eb615f
fredsonmartins <fredsonf24@gmail.com>
2025-05-01 17:33:54 +0000

    modificado

--- desafio.c
+++ desafio.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
 int main(){
+   
+    //carta 1
 
 char estado[30] ="goias";
 char codigodacarta[20] ="G";
-char nome cidade[20] ="G01";
-int Populacao = 3000;
-float area = 1.300;
-float pib = 1.500;
-int pontos turisticos = 600;
+//char nomedacidade[20] ="G01";
+//int Populacao = 3000;
+//float area = 1.300;
+//float pib = 1.500;
+//int pontosturisticos = 600;
+
+printf("O estado e: %c\s", estado);
+printf("o codigo da carta e:%c\n", g);
 
 
-return o
+
+
+return 0;
+
 
 
 
